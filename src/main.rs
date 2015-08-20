db8010ee01faefbacdb9945aeec1325c4c63f622
Adam Reeve <adreeve@gmail.com>
2015-08-20 21:52:24 +1200

    Implement a main with command line parsing using clap

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+#[macro_use]
+extern crate clap;
+use clap::App;
+use std::process;
+use std::io::Write;
+
+fn run() -> Result<(), &'static str> {
+    let args = App::new("Game of Life")
+        .version(&crate_version!()[..])
+        .about("Plays Conway's Game of Life")
+        .args_from_usage(
+            "-w --width=[WIDTH] 'Sets the width of the game, default=40'
+             -h --height=[HEIGHT] 'Sets the height of the game, default=40'")
+        .get_matches();
+
+    let width: u32 = match args.value_of("WIDTH").unwrap_or("40").parse() {
+        Ok(val) => val,
+        Err(_) => {
+            return Err("Invalid width specified");
+        }
+    };
+    let height: u32 = match args.value_of("HEIGHT").unwrap_or("40").parse() {
+        Ok(val) => val,
+        Err(_) => {
+            return Err("Invalid height specified");
+        }
+    };
+
+    println!("Size: {} x {}", width, height);
+    Ok(())
+}
+
+fn main() {
+    match run() {
+        Ok(_) => (),
+        Err(err) => {
+            writeln!(&mut ::std::io::stderr(), "{}", err).unwrap();
+            process::exit(-1);
+        }
+    }
+}
