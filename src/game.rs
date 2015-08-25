2ca4cde67ab7d3ac14fe2786f32b6445a70a29af
Adam Reeve <adreeve@gmail.com>
2015-08-25 19:45:14 +1200

    Implement cell toggling

--- src/game.rs
+++ src/game.rs
@@ -35,26 +35,30 @@ impl GameState {
 pub fn run(mut world_state: life::WorldState) -> Result<(), String> {
     let mut term = Terminal::new().unwrap();
     let mut game_state = GameState::new();
+    let mut game_step = 0;
 
     tryterm!(term.clear());
     tryterm!(term.swap_buffers());
-    'run: for iter in 1.. {
+    'run: loop {
         tryterm!(term.clear());
         while let Some(Event::Key(ch)) = term.get_event(0).unwrap() {
             match ch {
                 'q' => break 'run,
                 'p' => { game_state.running = !game_state.running; }
+                ' ' => { toggle_cell(&game_state, &mut world_state); }
                 _   => {},
             }
         }
 
         if game_state.running {
             world_state = life::tick(world_state);
+            game_step += 1;
         }
 
         display_game(&mut term, &world_state);
+        display_cursor(&mut term, &game_state);
 
-        let iter_label = format!("Iteration: {}", iter);
+        let iter_label = format!("Iteration: {}", game_step);
         write_text(&mut term, &iter_label, 0, -3);
         let state_label = if game_state.running { "Running (P to pause)".to_string() } else { "Paused (P to run)".to_string() };
         write_text(&mut term, &state_label, 0, -2);
@@ -67,10 +71,21 @@ pub fn run(mut world_state: life::WorldState) -> Result<(), String> {
     Ok(())
 }
 
+fn toggle_cell(game_state: &GameState, world_state: &mut life::WorldState) {
+    let position = life::CellPosition {
+        x: game_state.cursor_x,
+        y: game_state.cursor_y,
+    };
+    match world_state.get_cell(position) {
+        life::CellState::Alive => { world_state.set_cell(position, life::CellState::Dead); },
+        life::CellState::Dead => { world_state.set_cell(position, life::CellState::Alive); },
+    }
+}
+
 fn display_game(term: &mut Terminal, world_state: &life::WorldState) {
     let (cols, rows) = term.size();
     for cell in world_state.alive_cells() {
-        if cell.x > 0 && cell.y > 0 {
+        if cell.x >= 0 && cell.y >= 0 {
             let xpos = cell.x as usize;
             let ypos = cell.y as usize;
             if xpos < cols && ypos < rows {
@@ -82,6 +97,16 @@ fn display_game(term: &mut Terminal, world_state: &life::WorldState) {
                 }
             }
         }
+    }
+}
+
+fn display_cursor(term: &mut Terminal, game_state: &GameState) {
+    let (_cols, _rows) = term.size();
+    let cols = _cols as i32;
+    let rows = _rows as i32;
+    if game_state.cursor_x >= 0 && game_state.cursor_x < cols &&
+            game_state.cursor_y >= 0 && game_state.cursor_y < rows {
+        term.set_cursor(game_state.cursor_x as usize, game_state.cursor_y as usize).unwrap();
     }
 }
 
